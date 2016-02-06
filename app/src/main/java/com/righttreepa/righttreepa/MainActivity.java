02ef4e27eb8a4b2469a6de091cbb25da5f0f4a6d
MGabriel <mina_gabriel84@hotmail.com>
2016-02-06 12:05:45 -0500

    register app for GCM service

--- app/src/main/java/com/righttreepa/righttreepa/MainActivity.java
+++ app/src/main/java/com/righttreepa/righttreepa/MainActivity.java
@@ -1,21 +1,28 @@
 package com.righttreepa.righttreepa;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(this);
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -26,6 +33,16 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        boolean sentToken = sharedPreferences
+                .getBoolean("TokenRegistered", false);
+        Log.d("RegIntentService", String.valueOf(sentToken));
+        // Start IntentService to register this application with GCM.
+        Intent intent = new Intent(this, RegistrationIntentService.class);
+        startService(intent);
+
+
     }
 
     @Override
---
+++ app/src/main/java/com/righttreepa/righttreepa/RegistrationIntentService.java
@@ -0,0 +1,49 @@
+package com.righttreepa.righttreepa;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
+
+import java.io.IOException;
+
+/**
+ * Created by mina on 2/6/16.
+ */
+public class RegistrationIntentService extends IntentService {
+    private static final String TAG = "RegIntentService";
+
+    public RegistrationIntentService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d(TAG, "On Handle");
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        InstanceID instanceID = InstanceID.getInstance(this);
+        try {
+            String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),
+                    GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+
+            //Send Token to our Server
+            //Implement this method to send any registration to your app's servers.
+            sendRegistrationToServer(token);
+
+            sharedPreferences.edit().putBoolean("TokenRegistered",true).apply();
+            Log.d(TAG,token);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            sharedPreferences.edit().putBoolean("TokenRegistered",false).apply();
+        }
+    }
+
+    private void sendRegistrationToServer(String token) {
+        // Add custom implementation, as needed.
+    }
+}
