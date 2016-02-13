20adbd34754cd93d891b8e675aee309f6cd75a65
MGabriel <mina_gabriel84@hotmail.com>
2016-02-13 08:58:57 -0500

    Handling fragment views events from AddNewTreeMain class
    * add Volley Lib\n * better handling for viewpager views event in AddNewTreePagerAdapter

--- app/src/main/java/com/righttreepa/righttreepa/AddNewTreeMain.java
+++ app/src/main/java/com/righttreepa/righttreepa/AddNewTreeMain.java
@@ -1,10 +1,17 @@
 package com.righttreepa.righttreepa;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class AddNewTreeMain extends AppCompatActivity {
 
@@ -15,13 +22,37 @@ public class AddNewTreeMain extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-        viewPager.setAdapter(new AddNewTreePagerAdapter(getSupportFragmentManager()));
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        AddNewTreePagerAdapter adapter = new AddNewTreePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabanim_tabs);
         tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if (tab.getPosition() == 0) {
+                    String fragmentName = "android:switcher:" + viewPager.getId() + ":" + tab.getPosition();
+                    android.support.v4.app.Fragment fragment = getSupportFragmentManager().findFragmentByTag(fragmentName);
+                    View view = fragment.getView();
+                    TextView textView = (TextView) view.findViewById(R.id.link_signup);
+                    textView.setText("Hello");
+                    Log.d("Tab", fragmentName);
 
+                }
+            }
 
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
 
     }
--- app/src/main/java/com/righttreepa/righttreepa/AddNewTreePagerAdapter.java
+++ app/src/main/java/com/righttreepa/righttreepa/AddNewTreePagerAdapter.java
@@ -3,21 +3,25 @@ package com.righttreepa.righttreepa;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.SparseArray;
+import android.view.ViewGroup;
 
 /**
  * Created by mina on 2/9/16.
  */
 public class AddNewTreePagerAdapter extends FragmentPagerAdapter {
 
+    SparseArray<Fragment> registeredFragments = new SparseArray<Fragment>();
+    private String tabTitles[] = new String[]{"General", "Photos", "Location", "Categories", "Sub-Categories", "Submit"};
 
-    private String tabTitles[] = new String[]{"General", "Photos", "Location", "Categories","Sub-Categories","Submit"};
     public AddNewTreePagerAdapter(FragmentManager fm) {
         super(fm);
     }
 
     @Override
     public Fragment getItem(int position) {
-        switch (position){
+        switch (position) {
             case 0:
                 return new AddNewTreeTab0();
             case 1:
@@ -40,8 +44,19 @@ public class AddNewTreePagerAdapter extends FragmentPagerAdapter {
     public int getCount() {
         return tabTitles.length;
     }
+
     @Override
     public CharSequence getPageTitle(int position) {
         return tabTitles[position];
+    }
+    public Fragment getActiveFragment(FragmentManager fragmentManager, int position,ViewPager container) {
+        String name = makeFragmentName(container.getId(), position);
+
+        final Fragment fragmentByTag =  fragmentManager.findFragmentByTag(name);
+        return fragmentByTag;
+    }
+
+    private static String makeFragmentName(int viewId, int index) {
+        return "android:switcher:" + viewId + ":" + index;
     }
 }
