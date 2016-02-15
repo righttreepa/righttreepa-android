439e2710d2c868e6a1af2cd4d72e4e75e05cb776
Henry J Schmale <hschmale16@gmail.com>
2016-02-15 17:50:46 -0500

    Got the Maps Activity Working
    Added the maps view fragement. Added the api key to the repo. Got the view
    working. It will display the maps view now.

--- app/src/main/java/com/righttreepa/righttreepa/MainActivity.java
+++ app/src/main/java/com/righttreepa/righttreepa/MainActivity.java
@@ -15,9 +15,12 @@ import android.view.MenuItem;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
-
-
-
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity {
     SharedPreferences sharedPreferences;
